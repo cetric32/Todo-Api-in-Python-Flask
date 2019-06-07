ee6043cd6a22b009730a764e685db7ad657032ad
cetric <cetric32@gmail.com>
2019-06-07 15:06:20 +0300

    my Flask todo api modifications

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -26,31 +25,66 @@ class Todo(db.Model):
 
 @app.route('/user/', methods=['GET'])
 def get_all_users():
-    return ''
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+    return jsonify({'users': output})
 
 
-@app.route('/user/<user_id>',methods=['GET'])
-def get_one_user():
-    return ''
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({'message': "No user found"})
+
+    user_data = {}
+    user_data['id'] = user.id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    return jsonify({'user': user_data})
+
 
 
 @app.route('/user/', methods=['POST'])
 def create_user():
     data = request.get_json()
-    hashed_password = generate_password_hash(data['password'],method='sha256')
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    #check if user exists
+    user = User.query.filter_by(name=data['name']).first()
+    if user:
+        return jsonify({'message': "User already exists,Try another name!"})
     new_user = User(name=data['name'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'message': "New User Created"})
+    return jsonify({'message': "New User Created!"})
 
 
-@app.route('/user/<user_id>',methods=['PUT'])
-def promote_user():
-    return ''
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def promote_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({'message': "No user found"})
 
-@app.route('/user/<user_id>',methods=['DELETE'])
-def delete_user():
-    return ''
+    user.admin = True
+    db.session.commit()
+    return jsonify({'message': 'User has been promoted'})
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({'message': "No user found"})
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'The user has been deleted'})
 
 
 
